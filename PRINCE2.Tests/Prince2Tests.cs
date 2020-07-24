524f77c421ba15372d184cd08a068901e95c0ca7
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2020-07-24 16:11:59 +0200

    Add Prince2 tests

---
+++ PRINCE2.Tests/Prince2Tests.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace PRINCE2.Tests
+{
+    public class Prince2Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
